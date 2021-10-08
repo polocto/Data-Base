8f5ed71bc435b4d7841198edcd61aab5f96e141b
mathisCAMARD <49272947+mathisCAMARD@users.noreply.github.com>
2021-10-08 21:31:05 +0200

    Revert "query ask"
    This reverts commit aead52654807f6f294d2699a437e8c09c4fb1102.

--- assignments/1-Assignment/assignment1.sql
+++ assignments/1-Assignment/assignment1.sql
@@ -1,24 +1,5 @@
--- ------------------------------------------------------
--- NOTE: DO NOT REMOVE OR ALTER ANY LINE FROM THIS SCRIPT
--- ------------------------------------------------------
-select
-  'Query 00' as '';
--- Show execution context
-select
-  current_date(),
-  current_time(),
-  user(),
-  database();
--- Conform to standard group by constructs
-set
-  session sql_mode = 'ONLY_FULL_GROUP_BY';
--- Write the SQL queries that return the information below:
-  -- Ecrire les requêtes SQL retournant les informations ci-dessous:
-select
-  'Query 01' as '';
---! The countries of residence the supplier had to ship products to in 2014
-  --! Les pays de résidence où le fournisseur a dû envoyer des produits en 2014
-SELECT
+-- Query 1
+ SELECT
   DISTINCT c.residence
 FROM
   orders o
@@ -26,10 +7,9 @@ FROM
 WHERE
   YEAR(o.odate) = 2014
   AND c.residence != "NULL";
-select
-  'Query 02' as '';
---! For each known country of origin, its name, the number of products from that country, their lowest price, their highest price
-  --! Pour chaque pays d'orgine connu, son nom, le nombre de produits de ce pays, leur plus bas prix, leur plus haut prix
+
+-- Query 2
+
 SELECT
   p.origin,
   COUNT(*) AS numberOfProduct,
@@ -39,65 +19,29 @@ FROM
   products p
 GROUP BY
   p.origin;
-select
-  'Query 03' as '';
---! The customers who ordered in 2014 all the products (at least) that the customers named 'Smith' ordered in 2013
-  --! Les clients ayant commandé en 2014 tous les produits (au moins) commandés par les clients nommés 'Smith' en 2013
-SELECT
-  DISTINCT *
-FROM
-  customers NATURAL
-  JOIN orders
-WHERE
-  YEAR(odate) = 2014
-  AND pid IN (
-    SELECT
-      DISTINCT pid
-    FROM
-      orders NATURAL
-      JOIN customers
-    WHERE
-      cname = 'Smith'
-      AND YEAR(odate) = 2013
-  )
-GROUP BY
-  cid
-HAVING
-  COUNT(pid) >= (
-    SELECT
-      COUNT(DISTINCT pid)
-    FROM
-      orders NATURAL
-      JOIN customers
-    WHERE
-      cname = 'Smith'
-      AND YEAR(odate) = 2013
-  );
-select
-  'Query 04' as '';
---! For each customer and each product, the customer's name, the product's name, the total amount ordered by the customer for that product,
-  --! sorted by customer name (alphabetical order), then by total amount ordered (highest value first), then by product id (ascending order)
-  --! Par client et par produit, le nom du client, le nom du produit, le montant total de ce produit commandé par le client,
-  --! trié par nom de client (ordre alphabétique), puis par montant total commandé (plus grance valeur d'abord), puis par id de produit (croissant)
-SELECT
-  DISTINCT c.cname,
-  p.pname,
-  o.quantity * p.price AS price
-FROM
-  customers AS c NATURAL
-  JOIN orders AS o NATURAL
-  JOIN products AS p
-GROUP BY
-  cid,
-  pid
-ORDER BY
-  cname,
-  price DESC,
-  pid;
-select
-  'Query 05' as '';
---! The customers who only ordered products originating from their country
-  --! Les clients n'ayant commandé que des produits provenant de leur pays
+
+
+-- Query 3
+SELECT DISTINCT *
+FROM customers NATURAL JOIN orders
+WHERE YEAR(odate) = 2014 AND pid IN
+(
+    SELECT DISTINCT pid
+    FROM orders NATURAL JOIN customers
+    WHERE cname='Smith' AND YEAR(odate) = 2013
+) GROUP BY cid
+HAVING COUNT(pid) >= (SELECT COUNT(DISTINCT pid) FROM orders NATURAL JOIN customers WHERE cname='Smith' AND YEAR(odate) = 2013);
+
+-- QUery 4
+SELECT DISTINCT c.cname, p.pname, o.quantity*p.price AS price
+FROM customers AS c
+NATURAL JOIN orders AS o 
+NATURAL JOIN products AS p
+GROUP BY cid, pid
+ORDER BY cname, price DESC, pid;
+
+
+-- Query 5
 SELECT
   DISTINCT c.*
 FROM
@@ -116,10 +60,8 @@ WHERE
     WHERE
       p.origin != c.residence
   );
-select
-  'Query 06' as '';
---! The customers who ordered only products originating from foreign countries
-  --! Les clients n'ayant commandé que des produits provenant de pays étrangers
+
+-- Query 6
 SELECT
   DISTINCT c.*
 FROM
@@ -138,61 +80,43 @@ WHERE
     WHERE
       p.origin = c.residence
   );
-select
-  'Query 07' as '';
---! The difference between 'USA' residents' per-order average quantity and 'France' residents' (USA - France)
-  --! La différence entre quantité moyenne par commande des clients résidant aux 'USA' et celle des clients résidant en 'France' (USA - France)
-SELECT
-  (
-    SELECT
-      AVG(quantity)
-    FROM(
-        SELECT
-          quantity
-        FROM
-          orders NATURAL
-          JOIN customers
-        WHERE
-          residence = "USA"
-      ) q1
-  ) - (
-    SELECT
-      AVG(quantity)
-    FROM(
-        SELECT
-          DISTINCT quantity
-        FROM
-          orders NATURAL
-          JOIN customers
-        WHERE
-          residence = 'France'
-      ) q2
-  );
-select
-  'Query 08' as '';
---! The products ordered throughout 2014, i.e. ordered each month of that year
-  --! Les produits commandés tout au long de 2014, i.e. commandés chaque mois de cette année
-SELECT
-  products.*
-FROM
-  products NATURAL
-  JOIN (
-    SELECT
-      pid,
-      COUNT(DISTINCT MONTH(odate)) AS months
-    FROM
-      orders
-    WHERE
-      YEAR(odate) = 2014
-    GROUP BY
-      pid
-  ) tabmonths
+
+-- Query 7
+SELECT (
+        SELECT AVG(quantity)
+        FROM(
+                SELECT quantity
+                FROM orders
+                NATURAL JOIN customers
+                WHERE residence = "USA"
+        )q1
+)
+-
+(
+        SELECT AVG(quantity)
+        FROM(
+                SELECT DISTINCT quantity
+                FROM orders
+                NATURAL JOIN customers
+                WHERE residence = 'France'
+        )q2
+);
+
+
+-- Query 8
+SELECT products.*
+FROM products
+NATURAL JOIN
+(
+    SELECT pid, COUNT(DISTINCT MONTH(odate)) AS months
+    FROM orders
+    WHERE YEAR(odate)=2014
+    GROUP BY pid
+) tabmonths
 WHERE
-  months = 12;
-select
-  'Query 09' as '';
---! The customers who ordered all the products that cost less than $5
-  --! Les clients ayant commandé tous les produits de moins de $5
+months =12;
+
+-- Query 9
 SELECT
   c.*
 FROM
@@ -212,10 +136,10 @@ HAVING
     WHERE
       products.price < 5
   );
-select
-  'Query 10' as '';
---! The customers who ordered the greatest number of common products. Display 3 columns: cname1, cname2, number of common products, with cname1 < cname2
-  --! Les clients ayant commandé le grand nombre de produits commums. Afficher 3 colonnes : cname1, cname2, nombre de produits communs, avec cname1 < cname2
+
+
+-- Query 10
+
 SELECT
   cname1,
   cname2,
@@ -258,7 +182,7 @@ HAVING
           ) tab1
           INNER JOIN (
             SELECT
-              DISTINCT o.pid,
+              DISTINCT  o.pid,
               c.cname AS cname2
             FROM
               customers c NATURAL
@@ -270,10 +194,10 @@ HAVING
           tab2.cname2
       ) q
   );
-select
-  'Query 11' as '';
---! The customers who ordered the largest number of products
-  --! Les clients ayant commandé le plus grand nombre de produits
+
+
+-- Query 11
+
 SELECT
   c.*
 FROM
@@ -298,40 +222,34 @@ HAVING
           c.cid
       ) q
   );
-select
-  'Query 12' as '';
---! The products ordered by all the customers living in 'France'
-  --! Les produits commandés par tous les clients vivant en 'France'
-SELECT
-  DISTINCT p.*
+
+-- Query 12
+SELECT DISTINCT
+  p.*
 FROM
   customers c
   INNER JOIN orders o ON c.cid = o.cid
   INNER JOIN products p ON o.pid = p.pid
 WHERE
   c.residence = "FRANCE";
-select
-  'Query 13' as '';
---! The customers who live in the same country customers named 'Smith' live in (customers 'Smith' not shown in the result)
-  --! Les clients résidant dans les mêmes pays que les clients nommés 'Smith' (en excluant les Smith de la liste affichée)
+
+-- Query 13
 SELECT
   c.cname
 FROM
   customers c
-WHERE
-  c.residence IN (
+WHERE c.residence IN
+  (
     SELECT
       c.residence
     FROM
       customers c
     WHERE
       c.cname = "Smith"
-  )
+  )  
   AND c.cname != "Smith";
-select
-  'Query 14' as '';
---! The customers who ordered the largest total amount in 2014
-  --! Les clients ayant commandé pour le plus grand montant total sur 2014
+
+-- Query 14
 SELECT
   c.*
 FROM
@@ -359,10 +277,8 @@ HAVING
           c.cid
       ) q
   );
-select
-  'Query 15' as '';
---! The products with the largest per-order average amount
-  --! Les produits dont le montant moyen par commande est le plus élevé
+
+-- Query 15
 SELECT
   table1.pid,
   table1.pname,
@@ -403,10 +319,8 @@ HAVING
           pid
       ) q
   );
-select
-  'Query 16' as '';
---! The products ordered by the customers living in 'USA'
-  --! Les produits commandés par les clients résidant aux 'USA'
+
+-- Query 16
 SELECT
   DISTINCT p.*
 FROM
@@ -415,43 +329,34 @@ FROM
   JOIN products p
 WHERE
   c.residence = "USA";
-select
-  'Query 17' as '';
---! The pairs of customers who ordered the same product en 2014, and that product. Display 3 columns: cname1, cname2, pname, with cname1 < cname2
-  --! Les paires de client ayant commandé le même produit en 2014, et ce produit. Afficher 3 colonnes : cname1, cname2, pname, avec cname1 < cname2
-SELECT
-  cname1,
-  cname2,
-  pname
+
+
+-- Query 17
+SELECT  cname1, cname2, pname
+
 FROM
-  (
-    SELECT
-      DISTINCT cname AS cname1,
-      pid,
-      pname
-    FROM
-      customers NATURAL
-      JOIN orders NATURAL
-      JOIN products
-    WHERE
-      YEAR(odate) = 2014
-  ) table1
-  INNER JOIN (
-    SELECT
-      DISTINCT cname AS cname2,
-      pid
-    FROM
-      customers NATURAL
-      JOIN orders NATURAL
-      JOIN products
-    WHERE
-      YEAR(odate) = 2014
-  ) table2 ON table1.pid = table2.pid
-  AND table1.cname1 < table2.cname2;
-select
-  'Query 18' as '';
---! The products whose price is greater than all products from 'India'
-  --! Les produits plus chers que tous les produits d'origine 'India'
+
+(
+        SELECT DISTINCT cname AS cname1, pid, pname
+        FROM customers
+        NATURAL JOIN orders
+        NATURAL JOIN products
+        WHERE YEAR(odate)=2014
+        )table1
+
+INNER JOIN
+
+(
+        SELECT DISTINCT cname AS cname2, pid
+        FROM customers
+        NATURAL JOIN orders
+        NATURAL JOIN products
+        WHERE YEAR(odate)=2014
+        )table2
+
+ON table1.pid = table2.pid AND table1.cname1 < table2.cname2;
+
+-- Query 18
 SELECT
   p.*
 FROM
@@ -467,10 +372,9 @@ WHERE
     WHERE
       p.origin = "India"
   );
-select
-  'Query 19' as '';
---! The products ordered by the smallest number of customers (products never ordered are excluded)
-  --! Les produits commandés par le plus petit nombre de clients (les produits jamais commandés sont exclus)
+
+
+-- Query 19
 SELECT
   p.*
 FROM
@@ -494,37 +398,22 @@ HAVING
           p.pid
       ) q
   );
-select
-  'Query 20' as '';
---! For all countries listed in tables products or customers, including unknown countries: the name of the country, the number of customers living in this country, the number of products originating from that country
-  --! Pour chaque pays listé dans les tables products ou customers, y compris les pays inconnus : le nom du pays, le nombre de clients résidant dans ce pays, le nombre de produits provenant de ce pays
-SELECT
-  country,
-  MAX(numberCID) AS "Number of resident",
-  MAX(numberPID) AS "Number of product"
+
+-- Query 20
+
+SELECT country, MAX(numberCID) AS "Number of resident", MAX(numberPID) AS "Number of product"
 FROM
-  (
-    (
-      SELECT
-        origin AS country,
-        0 AS numberCID,
-        COUNT(DISTINCT pid) AS numberPID
-      FROM
-        products
-      GROUP BY
-        origin
-    )
-    UNION
-      (
-        SELECT
-          residence,
-          COUNT(DISTINCT cid),
-          0
-        FROM
-          customers
-        GROUP BY
-          residence
-      )
-  ) t
-GROUP BY
-  country;
+(
+(
+    SELECT origin AS country, 0 AS numberCID, COUNT(DISTINCT pid) AS numberPID
+    FROM products
+    GROUP BY origin
+)
+UNION
+(
+    SELECT residence, COUNT(DISTINCT cid), 0
+    FROM customers
+    GROUP BY residence
+)) t
+
+GROUP BY country;

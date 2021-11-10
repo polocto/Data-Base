6686025ccf927bbfdc26bd37af75f5b7dca96e05
Paul Sade <paul.sade@live.fr>
2021-11-10 13:21:58 +0100

    init lab 11.3

---
+++ lab/Lab11.3/queries.js
@@ -0,0 +1,54 @@
+//
+// Database: company.js
+//
+// Write the MongoDB queries that return the information below. Find as many solutions as possible for each query.
+//
+
+print("Query 01")
+// the highest salary of clerks
+
+print("Query 02")
+// the total salary of managers
+
+print("Query 03")
+// the lowest, average and highest salary of the employees
+
+print("Query 04")
+// the name of the departments
+
+print("Query 05")
+// for each job: the job and the average salary for that job
+
+print("Query 06")
+// for each department: its name, the number of employees and the average salary in that department (null departments excluded)
+
+print("Query 07")
+// the highest of the per-department average salary (null departments excluded)
+
+print("Query 08")
+// the name of the departments with at least 5 employees (null departments excluded)
+
+print("Query 09")
+// the cities where at least 2 missions took place
+
+print("Query 10")
+// the highest salary
+
+print("Query 11")
+// the name of _one_ of the employees with the highest salary
+
+print("Query 12")
+// the name of _all_ of the employees with the highest salary
+
+print("Query 13")
+// the name of the departments with the highest average salary
+
+print("Query 14")
+// for each city in which a mission took place, its name (output field "city") and the number of missions in that city
+
+print("Query 15")
+// the name of the employees who did a mission in the city they work in
+
+
+
+
